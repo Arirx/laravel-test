0e61a635deced3f16092fec49ac00628973941c4
Kirill <kirillmatuzko21@gmail.com>
2026-03-03 13:36:32 +0300

    add calculator controller

---
+++ app/Http/Controllers/CalculatorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CalculatorService;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class CalculatorController extends Controller
+{
+    public function __construct(private CalculatorService $service)
+    {
+        //
+    }
+    public function calculate(Request $request): JsonResponse
+    {
+        $request->validate([
+            'operation' => 'required|in:add,multiply',
+            'a' => 'required|integer',
+            'b' => 'required|integer'
+        ]);
+
+        $result = match ($request->operation) {
+            'add' => $this->service->add($request->a, $request->b),
+            'multiply' => $this->service->multiply($request->a, $request->b)
+        };
+
+        return response()->json(['result' => $result]);
+    }
+}
