8a016b91ae69a18269835a4834b1f9614c5b52c4
Kirill <kirillmatuzko21@gmail.com>
2026-03-03 13:26:11 +0300

    add calculator service

---
+++ app/Service/CalculatorService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class CalculatorService
+{
+    public function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+
+    public function multiply(int $a, int $b): int
+    {
+        return $a * $b;
+    }
+}
